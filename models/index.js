552e13c497b7f8ac3af165d660863d12ae5e9294
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-09-30 14:25:44 -0600

    updated relationships between users and chats through message model

--- models/index.js
+++ models/index.js
@@ -2,6 +2,22 @@ const Chat = require("./Chat");
 const Message = require("./Message");
 const User = require("./User");
 
+Chat.belongsToMany(User, {
+  through: {
+    model: Message,
+    unique: false
+  },
+  as: 'user_chats'
+});
+
+User.belongsToMany(Chat, {
+  through: {
+    model: Message,
+    unique: false
+  },
+  as: 'chatrooms'
+});
+
 module.exports = {
   Chat,
   Message,
