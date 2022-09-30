85eae32723f8b5a819898fa3439b83ce0ab36f4b
ChrisCODES23 <christiantalledo89@gmail.com>
2022-09-30 11:02:41 -0700

    Added the handlebar files, added an env, added a session cookie

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(routes);
 
