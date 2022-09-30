05ef9f4a1bb4ecd479c8200da0fb762726083903
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-09-30 14:13:09 -0600

    reverted the home-routes back to controller folder

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { User } = require("../../models");
-const withAuth = require("../../utils/auth");
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
 // Get user
 
