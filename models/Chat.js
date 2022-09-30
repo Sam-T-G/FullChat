25a9db4a16cfa436a93d1a1ec6ffdb460df93148
ChrisCODES23 <christiantalledo89@gmail.com>
2022-09-30 11:44:30 -0700

    finished models

--- models/Chat.js
+++ models/Chat.js
@@ -1,7 +1,28 @@
 const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require ("../config/connection.js");
+const sequelize = require("../config/connection.js");
 
 class Chat extends Model {}
+
+Chat.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    chat_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "chat",
+  }
+);
 
 module.exports = Chat;
