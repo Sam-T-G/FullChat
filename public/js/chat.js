cd8f63b5e9f1abbbf18ab02d6124de2dc008dbb5
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2022-10-03 13:14:48 -0700

    Adding comments to app function workflow

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,14 +2,18 @@ var socket = io();
 var messages = document.getElementById("messages");
 var form = document.getElementById("form");
 var input = document.getElementById("input");
+
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (input.value) {
     socket.emit("chat message", input.value);
     input.value = "";
   }
+
+  //   Socket.io function to render messages on to application as "li"s
   socket.on("chat message", function (msg) {
-    console.log("we chattin'");
+    // console.log("we chattin'");
     var item = document.createElement("li");
     item.textContent = msg;
     messages.appendChild(item);
